0255920c703ff71763f9dcb69d28aae2c8b93200
Mathieu Vieillefont <57585607+Mamath79@users.noreply.github.com>
2024-11-25 15:29:39 +0100

    Etape 2 , ok . model user plus test api ok

--- users/models.py
+++ users/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+
 class User(AbstractUser):
 
-    age = models.IntegerField(null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
     can_be_contacted = models.BooleanField(default=False)
     can_data_be_shared = models.BooleanField(default=False)
-
--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from users.models import User
+from datetime import date
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -8,21 +9,31 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id',
                  'username',
                  'email',
-                 'age',
+                 'password',
+                 'birth_date',
                  'can_be_contacted',
                  'can_data_be_shared',
                  ]
         extra_kargs = {
             'password': {'write_only': True}
         }
-    
+    def validate_age(self, value):
+        if value:
+            today = date.today()
+            age = today.year - value.year -((today.month, today.day) < (value.month, value.day))
+        if age < 15:
+            raise serializers.ValidationError("L'âge doit être supérieur ou égal à 15 ans.")
+        return value
+
     def create(self, validated_data):
         user = User.objects.create_user(
-            username=validated_data['user_name'],
+            username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password'],
-            age=validated_data.get('age'),
+            birth_date=validated_data.get('birth_date'),
             can_be_contacted=validated_data.get('can_be_contacted',False),
             can_data_be_shared=validated_data.get('can_data_be_shared',False),
         )
         return user
+    
+    
